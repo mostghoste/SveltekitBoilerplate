49eaa4b9e1769b5aa20494427d510ba12e25f09d
mostghoste <sarunas.jacikas+github@gmail.com>
2024-07-23 11:38:47 +0300

    Add image loading for products

--- src/routes/products/ProductCard.svelte
+++ src/routes/products/ProductCard.svelte
@@ -4,7 +4,10 @@
 
 {#if Product}
 	<article>
-		<img src={Product.image} alt="Part {Product.part_code}" />
+		<img
+			src="https://tlsgwucpdiwudwghrljn.supabase.co/storage/v1/object/public/product_images/{Product.image}"
+			alt="Part {Product.part_code}"
+		/>
 		<h3>{Product.part_name}</h3>
 		<h4>{Product.part_code}</h4>
 		<p>Price: <span>{Product.price.toFixed(2)}</span></p>
