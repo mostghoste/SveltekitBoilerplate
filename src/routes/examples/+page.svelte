556f6245b1270ed0a752597506960eaadeda8b3b
mostghoste <sarunas.jacikas+github@gmail.com>
2024-07-23 12:00:49 +0300

    Add and configure daisyui

--- src/routes/examples/+page.svelte
+++ src/routes/examples/+page.svelte
@@ -3,9 +3,12 @@
 	export let data;
 </script>
 
-<h1>Examples</h1>
-<p>It should be safe to delete the whole Examples route.</p>
+<h1 class="">Examples</h1>
+<p class="p">It should be safe to delete the whole Examples route.</p>
 
 <h2>Loading data</h2>
 <p>This data is loaded server side in +page.server.js</p>
 <p>Raw data: {JSON.stringify(data)}</p>
+
+<h2>Styling</h2>
+<p>This boilerplate uses tailwindcss and daisyUI for styling</p>
