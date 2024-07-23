680d922732eee129a77a4310383963b786feca49
mostghoste <sarunas.jacikas+github@gmail.com>
2024-07-23 12:38:56 +0300

    Change landing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Agrobond wholesale</h1>
+<a class="btn btn-primary" href="/products">Products</a>
