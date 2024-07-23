28489bc02789e66793aec1a9e71bffef93e4cec1
mostghoste <sarunas.jacikas+github@gmail.com>
2024-07-23 12:14:53 +0300

    Add buttons to example

--- src/routes/examples/+page.svelte
+++ src/routes/examples/+page.svelte
@@ -3,12 +3,31 @@
 	export let data;
 </script>
 
-<h1 class="">Examples</h1>
-<p class="p">It should be safe to delete the whole Examples route.</p>
+<article class="p-4 flex flex-col gap-4">
+	<div>
+		<h1 class="font-bold text-2xl">Examples</h1>
+		<p class="p">It should be safe to delete the whole Examples route.</p>
+	</div>
 
-<h2>Loading data</h2>
-<p>This data is loaded server side in +page.server.js</p>
-<p>Raw data: {JSON.stringify(data)}</p>
+	<div>
+		<h2 class="font-bold text-xl">Loading data</h2>
+		<p>This data is loaded server side in +page.server.js</p>
+		<p>Raw data: {JSON.stringify(data)}</p>
+	</div>
 
-<h2>Styling</h2>
-<p>This boilerplate uses tailwindcss and daisyUI for styling</p>
+	<div>
+		<h2 class="font-bold text-xl">Styling</h2>
+		<p>This boilerplate uses tailwindcss and daisyUI for styling.</p>
+	</div>
+
+	<div>
+		<h3 class="font-bold text-l">Buttons</h3>
+		<button class="btn">Button</button>
+		<button class="btn btn-neutral">Neutral</button>
+		<button class="btn btn-primary">Primary</button>
+		<button class="btn btn-secondary">Secondary</button>
+		<button class="btn btn-accent">Accent</button>
+		<button class="btn btn-ghost">Ghost</button>
+		<button class="btn btn-link">Link</button>
+	</div>
+</article>
