f4120a3a363fea7a58bc2569cd08a15c821a19f9
mostghoste <sarunas.jacikas+github@gmail.com>
2024-07-25 16:05:40 +0300

    Add invite user form

--- src/routes/admin/users/+page.server.js
+++ src/routes/admin/users/+page.server.js
@@ -1,3 +1,50 @@
+import { fail } from '@sveltejs/kit';
+import { createClient } from '@supabase/supabase-js'
+import { PUBLIC_SUPABASE_URL } from '$env/static/public'
+import { SUPABASE_SERVICE_KEY } from '$env/static/private'
+
+
+export const actions = {
+  invite: async ({ request }) => {
+    console.log("Attempting to invite")
+    const data = await request.formData();
+    const email = data.get('email');
+    const first_name = data.get('first_name');
+    const last_name = data.get('last_name');
+    const company = data.get('company');
+    const phone = data.get('phone');
+
+    if (!email) {
+        console.log("Error: No email provided")
+      return fail(400, { error: 'Email is required' });
+    }
+
+    const supabase = createClient(PUBLIC_SUPABASE_URL, SUPABASE_SERVICE_KEY, {
+        auth: {
+          autoRefreshToken: false,
+          persistSession: false
+        }
+      })
+
+    const { data: user, error } = await supabase.auth.admin.inviteUserByEmail(email, {
+      first_name,
+      last_name,
+      company,
+      phone,
+    });
+
+    if (error) {
+        console.log("Error: " + error.message);
+        console.log(JSON.stringify(error))
+      return fail(500, { error: error.message });
+    } else {
+        console.log("User: " + user)
+    }
+
+    return { success: true };
+  }
+};
+
 export const load = async ({ locals }) => {
     const supabase = locals.supabase;
   
--- src/routes/admin/users/+page.svelte
+++ src/routes/admin/users/+page.svelte
@@ -3,30 +3,59 @@
 	$: ({ supabase, user, role, users } = data);
 </script>
 
-<h1>All Users</h1>
-<table class="table">
-	<thead>
-		<tr>
-			<th>Email</th>
-			<th>Name</th>
-			<th>Company</th>
-			<th>Customer Group</th>
-			<th>Status</th>
-		</tr>
-	</thead>
-	<tbody>
-		{#each users as user}
+<main class="flex flex-col gap-2 m-2">
+	<h1 class="font-bold">All Users</h1>
+	<table class="table">
+		<thead>
 			<tr>
-				<td>{user.role === 'admin' ? '👑 ' : ''}{user.email}</td>
-				<td
-					>{user.first_name || user.last_name
-						? `${user.first_name || ''} ${user.last_name || ''}`
-						: '-'}</td
-				>
-				<td>{user.company || '-'}</td>
-				<td>{user.customer_group || 'Default'}</td>
-				<td>{user.status || 'Unknown'}</td>
+				<th>Email</th>
+				<th>Name</th>
+				<th>Company</th>
+				<th>Customer Group</th>
+				<th>Status</th>
 			</tr>
-		{/each}
-	</tbody>
-</table>
+		</thead>
+		<tbody>
+			{#each users as user}
+				<tr>
+					<td>{user.role === 'admin' ? '👑 ' : ''}{user.email}</td>
+					<td
+						>{user.first_name || user.last_name
+							? `${user.first_name || ''} ${user.last_name || ''}`
+							: '-'}</td
+					>
+					<td>{user.company || '-'}</td>
+					<td>{user.customer_group || 'Default'}</td>
+					<td>{user.status || 'Unknown'}</td>
+				</tr>
+			{/each}
+			<tr><td class="font-bold">Total users: {users.length}</td></tr>
+		</tbody>
+	</table>
+
+	<!-- Form to invite a new user -->
+	<form method="post" action="?/invite" class="flex gap-2 flex-col p-2 border w-96">
+		<h2 class="font-bold">Invite a user</h2>
+		<div class="flex flex-col gap-1">
+			<label for="email">Email <span class="text-red-500">*</span></label>
+			<input class="input input-bordered" type="email" id="email" name="email" required />
+		</div>
+		<div class="flex flex-col gap-1">
+			<label for="first_name">First Name</label>
+			<input class="input input-bordered" type="text" id="first_name" name="first_name" />
+		</div>
+		<div class="flex flex-col gap-1">
+			<label for="last_name">Last Name</label>
+			<input class="input input-bordered" type="text" id="last_name" name="last_name" />
+		</div>
+		<div class="flex flex-col gap-1">
+			<label for="company">Company</label>
+			<input class="input input-bordered" type="text" id="company" name="company" />
+		</div>
+		<div class="flex flex-col gap-1">
+			<label for="phone">Phone</label>
+			<input class="input input-bordered" type="tel" id="phone" name="phone" />
+		</div>
+		<button class="btn btn-success" type="submit">Invite User</button>
+	</form>
+</main>
