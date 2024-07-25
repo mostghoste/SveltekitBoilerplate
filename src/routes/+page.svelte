ec259ff887cfce90cff8c99cc758f4ceeae9e1fb
mostghoste <sarunas.jacikas+github@gmail.com>
2024-07-25 09:51:41 +0300

    Modify landing design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Agrobond wholesale</h1>
-<a class="btn btn-primary" href="/products">Products</a>
+<script>
+	import AgrobondLogo from '$lib/assets/images/logo.png';
+	import AuthForm from '$lib/components/AuthForm.svelte';
+</script>
+
+<main class="flex justify-center items-center flex-col h-screen gap-2">
+	<figure class="max-w-64 flex items-center gap-2 justify-center">
+		<img src={AgrobondLogo} alt="Agrobond logo" />
+	</figure>
+	<form method="post" action="/auth?/login" class="flex flex-col gap-2 border p-4 px-8">
+		<input type="email" name="email" class="input input-bordered" placeholder="Email" />
+		<input type="password" name="password" class="input input-bordered" placeholder="Password" />
+		<button type="submit" class="btn btn-success">Log in</button>
+	</form>
+</main>
