3fc17496076c10f39caf49f193ad0544cce096b0
mostghoste <sarunas.jacikas+github@gmail.com>
2024-07-29 14:12:09 +0300

    Add loading of groups

--- src/routes/admin/customer_groups/+page.server.js
+++ src/routes/admin/customer_groups/+page.server.js
@@ -1,4 +1,19 @@
 /** @type {import('./$types').PageServerLoad} */
-export async function load() {
-    return {};
-};
+export const load = async ({ locals }) => {
+    const supabase = locals.supabase;
+  
+    // Fetch customer groups
+    const { data: customerGroups, error: customerGroupsError } = await supabase
+      .from('customer_groups')
+      .select('*');
+  
+    if (customerGroupsError) {
+      console.error('Error fetching customer groups:', customerGroupsError);
+      return { customerGroups: [] };
+    }
+  
+    return {
+      customerGroups,
+    };
+  };
+  
--- src/routes/admin/customer_groups/+page.svelte
+++ src/routes/admin/customer_groups/+page.svelte
@@ -4,3 +4,23 @@
 </script>
 
 <h1 class="font-bold">Manage customer groups</h1>
+<table class="table w-full">
+	<thead>
+		<tr>
+			<th class="">ID</th>
+			<th class="">Group Name</th>
+			<th class="">Group Description</th>
+			<!-- <th class="">Created At</th> -->
+		</tr>
+	</thead>
+	<tbody>
+		{#each data.customerGroups as group}
+			<tr>
+				<td class="">{group.id}</td>
+				<td class="">{group.group_name}</td>
+				<td class="">{group.group_description || 'No description'}</td>
+				<!-- <td class="">{new Date(group.created_at).toLocaleString()}</td> -->
+			</tr>
+		{/each}
+	</tbody>
+</table>
