53ffcf8ca0ca235628623f67dcc4e769b6c1422f
mostghoste <sarunas.jacikas+github@gmail.com>
2024-08-01 15:29:29 +0300

    Tweak navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,38 +28,76 @@
 </script>
 
 {#if user}
-	<nav class="bg-base-200 flex p-2 justify-between">
-		<div class="flex">
-			<a class="max-w-24 flex items-center justify-center mx-4" href="/products">
-				<img src={AgrobondLogo} alt="Agrobond logo" />
+	<div class="navbar bg-base-200">
+		<div class="flex-1">
+			<a class="btn btn-ghost normal-case text-xl flex items-center" href="/products">
+				<img src={AgrobondLogo} alt="Agrobond logo" class="max-w-24 mr-2" />
 			</a>
-			<ul class="flex">
-				<li>
-					<a class="btn btn-ghost" href="/products">Products</a>
-				</li>
-				{#if role === 'admin'}
-					<li>
-						<a class="btn btn-ghost" href="/admin/users">Manage users</a>
-					</li>
-					<li>
-						<a class="btn btn-ghost" href="/admin/products">Manage products</a>
-					</li>
-					<li>
-						<a class="btn btn-ghost" href="/admin/customer_groups">Manage customer groups</a>
-					</li>
-					<li>
-						<a class="btn btn-ghost" href="/admin/categories">Manage categories</a>
-					</li>
-				{/if}
-			</ul>
 		</div>
-		<button
-			on:click={() => {
-				logout();
-			}}
-			class="btn btn-warning">Log out</button
-		>
-	</nav>
+
+		<li class="btn btn-ghost"><a href="/products">Products</a></li>
+		{#if role === 'admin'}
+			<li class="btn btn-ghost"><a href="/admin/users">Manage users</a></li>
+			<li class="btn btn-ghost"><a href="/admin/products">Manage products</a></li>
+			<li class="btn btn-ghost"><a href="/admin/customer_groups">Manage customer groups</a></li>
+			<li class="btn btn-ghost"><a href="/admin/categories">Manage categories</a></li>
+		{/if}
+
+		<div class="flex-none">
+			<div class="dropdown dropdown-end">
+				<div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+					<div class="indicator">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-5 w-5"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+							/>
+						</svg>
+						<span class="badge badge-sm indicator-item">8</span>
+					</div>
+				</div>
+				<div
+					tabindex="0"
+					class="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
+				>
+					<div class="card-body">
+						<span class="text-lg font-bold">8 Items</span>
+						<span class="text-info">Subtotal: $999</span>
+						<div class="card-actions">
+							<button class="btn btn-primary btn-block">View cart</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="dropdown dropdown-end">
+				<div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar placeholder">
+					<div class="w-10 rounded-full bg-neutral text-neutral-content">
+						<span class="text-xl">{user.email.slice(0, 2)}</span>
+					</div>
+				</div>
+				<ul
+					tabindex="0"
+					class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+				>
+					<!-- <li>
+						<a href="/profile">Profile</a>
+					</li>
+					<li><a href="/settings">Settings</a></li> -->
+
+					<li><a on:click={logout}>Logout</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
 {/if}
 
 <slot></slot>
