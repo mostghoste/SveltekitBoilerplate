5caf51a834465441b78baf8e88dad23513dcb1f9
mostghoste <sarunas.jacikas+github@gmail.com>
2024-08-01 15:32:15 +0300

    Fix accessibility issues

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -45,7 +45,7 @@
 
 		<div class="flex-none">
 			<div class="dropdown dropdown-end">
-				<div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+				<button class="btn btn-ghost btn-circle">
 					<div class="indicator">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
@@ -63,11 +63,8 @@
 						</svg>
 						<span class="badge badge-sm indicator-item">8</span>
 					</div>
-				</div>
-				<div
-					tabindex="0"
-					class="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
-				>
+				</button>
+				<div class="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
 					<div class="card-body">
 						<span class="text-lg font-bold">8 Items</span>
 						<span class="text-info">Subtotal: $999</span>
@@ -79,21 +76,19 @@
 			</div>
 
 			<div class="dropdown dropdown-end">
-				<div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar placeholder">
+				<button class="btn btn-ghost btn-circle avatar placeholder">
 					<div class="w-10 rounded-full bg-neutral text-neutral-content">
 						<span class="text-xl">{user.email.slice(0, 2)}</span>
 					</div>
-				</div>
+				</button>
 				<ul
-					tabindex="0"
 					class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
 				>
 					<!-- <li>
 						<a href="/profile">Profile</a>
 					</li>
 					<li><a href="/settings">Settings</a></li> -->
-
-					<li><a on:click={logout}>Logout</a></li>
+					<li><button on:click={logout}>Logout</button></li>
 				</ul>
 			</div>
 		</div>
