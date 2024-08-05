c017d95fad39bc76da64708b1fa8733c59f50ae8
mostghoste <sarunas.jacikas+github@gmail.com>
2024-08-05 16:14:58 +0300

    Add pagination translation

--- src/routes/admin/products/PaginationControl.svelte
+++ src/routes/admin/products/PaginationControl.svelte
@@ -1,4 +1,6 @@
 <script>
+	import * as m from '$lib/paraglide/messages.js';
+
 	export let page;
 	export let totalPages;
 	export let totalCount;
@@ -6,12 +8,12 @@
 </script>
 
 <div class="flex justify-between items-center">
-	<span>Total Products: {totalCount}</span>
+	<span>{m.total_products({ totalCount })}</span>
 	<div class="flex items-center gap-2">
 		<button class="btn btn-sm btn-accent" disabled={page === 1} on:click={() => goToPage(page - 1)}>
 			{'<'}
 		</button>
-		<span>Page {page} of {totalPages}</span>
+		<span>{m.pagination_info({ page, totalPages })}</span>
 		<button
 			class="btn btn-sm btn-accent"
 			disabled={page === totalPages}
