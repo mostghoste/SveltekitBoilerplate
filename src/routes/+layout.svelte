138a4dc409ed4db75d9fa929e8b4a759bd68833d
mostghoste <sarunas.jacikas+github@gmail.com>
2024-08-05 15:07:26 +0300

    Add translations for navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
-	import { ParaglideJS } from '@inlang/paraglide-sveltekit'
-	import { i18n } from '$lib/i18n'
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import { i18n } from '$lib/i18n';
+	import * as m from '$lib/paraglide/messages.js';
 
 	import '../app.css';
 	import { invalidate, invalidateAll } from '$app/navigation';
@@ -13,9 +14,7 @@
 	let isPing = false; // State for ping animation
 	let pingTimeout; // Timeout for removing the animation class
 
-	// Subscribe to the cart store
 	cart.subscribe((value) => {
-		// If the cart length increases, trigger the ping animation
 		if (value.length > CartProducts.length) {
 			isPing = true;
 			clearTimeout(pingTimeout);
@@ -47,72 +46,81 @@
 		invalidateAll();
 	};
 </script>
+
 <ParaglideJS {i18n}>
+	{#if user}
+		<nav class="navbar bg-base-200 sticky top-0 shadow-md z-50">
+			<div class="flex-1">
+				<a
+					class="btn btn-ghost normal-case text-xl flex items-center"
+					href={i18n.resolveRoute('/products')}
+				>
+					<img src={AgrobondLogo} alt="Agrobond logo" class="max-w-24 mr-2" />
+				</a>
+			</div>
 
+			<ul class="flex space-x-2">
+				<li class="btn btn-ghost"><a href={i18n.resolveRoute('/products')}>{m.products()}</a></li>
+				{#if role === 'admin'}
+					<li class="btn btn-ghost">
+						<a href={i18n.resolveRoute('/admin/users')}>{m.manage_users()}</a>
+					</li>
+					<li class="btn btn-ghost">
+						<a href={i18n.resolveRoute('/admin/products')}>{m.manage_products()}</a>
+					</li>
+					<li class="btn btn-ghost">
+						<a href={i18n.resolveRoute('/admin/customer_groups')}>{m.manage_customer_groups()}</a>
+					</li>
+					<li class="btn btn-ghost">
+						<a href={i18n.resolveRoute('/admin/categories')}>{m.manage_categories()}</a>
+					</li>
+				{/if}
+			</ul>
 
-{#if user}
-	<nav class="navbar bg-base-200 sticky top-0 shadow-md z-50">
-		<div class="flex-1">
-			<a class="btn btn-ghost normal-case text-xl flex items-center" href="/products">
-				<img src={AgrobondLogo} alt="Agrobond logo" class="max-w-24 mr-2" />
-			</a>
-		</div>
-
-		<ul class="flex space-x-2">
-			<li class="btn btn-ghost"><a href="/products">Products</a></li>
-			{#if role === 'admin'}
-				<li class="btn btn-ghost"><a href="/admin/users">Manage users</a></li>
-				<li class="btn btn-ghost"><a href="/admin/products">Manage products</a></li>
-				<li class="btn btn-ghost"><a href="/admin/customer_groups">Manage customer groups</a></li>
-				<li class="btn btn-ghost"><a href="/admin/categories">Manage categories</a></li>
-			{/if}
-		</ul>
-
-		<div class="flex-none">
-			<div class="dropdown dropdown-end">
-				<button class="btn btn-ghost btn-circle relative">
-					<div class="indicator">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							class="h-5 w-5"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke="currentColor"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-							/>
-						</svg>
-						<span class="badge badge-sm indicator-item">{CartProducts.length || 0}</span>
+			<div class="flex-none">
+				<div class="dropdown dropdown-end">
+					<button class="btn btn-ghost btn-circle relative">
+						<div class="indicator">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								class="h-5 w-5"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke="currentColor"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+								/>
+							</svg>
+							<span class="badge badge-sm indicator-item">{CartProducts.length || 0}</span>
+						</div>
+						{#if isPing}
+							<span class="absolute inset-0 animate-ping rounded-full bg-success opacity-75"></span>
+						{/if}
+					</button>
+					<div class="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-72 shadow">
+						<CartPreviewSmall {user}></CartPreviewSmall>
 					</div>
-					{#if isPing}
-						<span class="absolute inset-0 animate-ping rounded-full bg-success opacity-75"></span>
-					{/if}
-				</button>
-				<div class="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-72 shadow">
-					<CartPreviewSmall {user}></CartPreviewSmall>
+				</div>
+
+				<div class="dropdown dropdown-end">
+					<button class="btn btn-ghost btn-circle avatar placeholder">
+						<div class="w-10 rounded-full bg-neutral text-neutral-content">
+							<span class="text-xl">{user.email.slice(0, 2)}</span>
+						</div>
+					</button>
+					<ul
+						class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+					>
+						<li><button on:click={logout}>{m.logout()}</button></li>
+					</ul>
 				</div>
 			</div>
+		</nav>
+	{/if}
 
-			<div class="dropdown dropdown-end">
-				<button class="btn btn-ghost btn-circle avatar placeholder">
-					<div class="w-10 rounded-full bg-neutral text-neutral-content">
-						<span class="text-xl">{user.email.slice(0, 2)}</span>
-					</div>
-				</button>
-				<ul
-					class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-				>
-					<li><button on:click={logout}>Logout</button></li>
-				</ul>
-			</div>
-		</div>
-	</nav>
-{/if}
-
-<slot></slot>
-
+	<slot></slot>
 </ParaglideJS>
