4c07cc2210cd57fc91d53ee10978651ea3fcfaae
mostghoste <sarunas.jacikas@gmail.com>
2025-02-11 20:46:32 +0200

    Add frontend for handling file upload

--- src/routes/admin/excel_upload/+page.svelte
+++ src/routes/admin/excel_upload/+page.svelte
@@ -3,6 +3,24 @@
 
 	/** @type {import('./$types').PageData} */
 	export let data;
+	let file;
+
+	const handleSubmit = async () => {
+		const formData = new FormData();
+		formData.append('file', file);
+
+		const response = await fetch('/excel_upload', {
+			method: 'POST',
+			body: formData
+		});
+
+		const result = await response.json();
+		if (result.success) {
+			alert('Data imported successfully!');
+		} else {
+			alert('Failed to import data.');
+		}
+	};
 </script>
 
 <h1 class="font-bold">Excel upload</h1>
@@ -12,7 +30,18 @@
 <p><b>{431}</b> active listings in <b>{18}</b> categories</p>
 <p><b>{6}</b> client groups</p>
 
-<footer>
-	<button class="btn btn-secondary">Export database to excel</button>
-	<button class="btn btn-secondary">Import database from excel</button>
-</footer>
+<main class="flex flex-col gap-8">
+	<section class="border border-black p-1 flex flex-col gap-4 max-w-96">
+		<h2 class="text-xl">Export (⚠️ Under construction)</h2>
+		<form action="">
+			<button class="btn btn-secondary" disabled>Export database to excel</button>
+		</form>
+	</section>
+	<section class="border border-black p-1 flex flex-col gap-4 max-w-96">
+		<h2 class="text-xl">Import</h2>
+		<form action="" class="flex flex-col w-fit gap-2">
+			<input type="file" bind:this={file} accept=".xlsx, .xls" />
+			<button class="btn btn-secondary" on:click={handleSubmit}>Import Data from excel</button>
+		</form>
+	</section>
+</main>
